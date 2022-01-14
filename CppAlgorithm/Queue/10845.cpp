5bbfba04b0f7bcd58d4d34441077c050e6650a1d
Administrator <Administrator@User-2021UJNGPH>
2022-01-14 14:53:11 +0900

    queue

---
+++ CppAlgorithm/Queue/10845.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    queue<int> q;
+    for (int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+        if (command == "push") {
+            int num;
+            cin >> num;
+            q.push(num);
+        }
+        else if (command == "pop") {
+            if (q.empty()) cout << -1 << '\n';
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (command == "size") {
+            cout << q.size() << '\n';
+        }
+        else if (command == "empty") {
+            cout << (int)(q.empty()) << '\n';
+        }
+        else if (command == "front") {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        }
+        else if (command == "back") {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+}
---
+++ CppAlgorithm/Queue/2164.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    queue<int> card;
+    for (int i = 1; i <= n; i++)
+        card.push(i);
+    while (card.size() != 1) {
+        card.pop();
+        card.push(card.front());
+        card.pop();
+    }
+    cout << card.front();
+}
