73cade1b4d2101de8f500802a205b56e31e0b315
Administrator <Administrator@User-2021UJNGPH>
2022-01-14 14:56:43 +0900

    deque

---
+++ CppAlgorithm/Deque/10866.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    deque<int> dq;
+    int n;
+    cin >> n;
+    while (n--) {
+        string command;
+        cin >> command;
+        if (command == "push_front") {
+            int num;
+            cin >> num;
+            dq.push_front(num);
+        }
+        else if (command == "push_back") {
+            int num;
+            cin >> num;
+            dq.push_back(num);
+        }
+        else if (command == "pop_front") {
+            if (dq.empty()) cout << -1 << '\n';
+            else {
+                cout << dq.front() << '\n';
+                dq.pop_front();
+            }
+        }
+        else if (command == "pop_back") {
+            if (dq.empty()) cout << -1 << '\n';
+            else {
+                cout << dq.back() << '\n';
+                dq.pop_back();
+            }
+        }
+        else if (command == "size") {
+            cout << dq.size() << '\n';
+        }
+        else if (command == "empty") {
+            cout << dq.empty() << '\n';
+        }
+        else if (command == "front") {
+            if (dq.empty()) cout << -1 << '\n';
+            else cout << dq.front() << '\n';
+        }
+        else if (command == "back") {
+            if (dq.empty()) cout << -1 << '\n';
+            else cout << dq.back() << '\n';
+        }
+    }
+}
