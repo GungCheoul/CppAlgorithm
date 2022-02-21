cde0add3933a57d47c4f6d0e807f4922fc99d96c
GungCheoul <jkc3556@gmail.com>
2022-02-21 10:07:26 +0900

    add comment

--- CppAlgorithm/Backtracking/15649.cpp
+++ CppAlgorithm/Backtracking/15649.cpp
@@ -7,17 +7,20 @@ bool isUsed[10];
 
 void func(int k) {
 	if (k == m) {
+		// 모든 칸이 찼을 때(트리 기준 마지막 노드에 도달)
+		// m개 만큼 한 줄에 출력
 		for (int i = 0; i < m; i++)
 			cout << arr[i] << ' ';
 		cout << '\n';
 		return;
 	}
+	// 1부터 n까지 반복
 	for (int i = 1; i <= n; i++) {
-		if (!isUsed[i]) {
-			arr[k] = i;
-			isUsed[i] = true;
-			func(k + 1);
-			isUsed[i] = false;
+		if (!isUsed[i]) { // 쓰이지 않은 경우
+			arr[k] = i; // 인덱스에 i값 저장
+			isUsed[i] = true; // i값 사용 표시
+			func(k + 1); // 다음, +1 함수 실행
+			isUsed[i] = false; // 들어갔다 나온 인덱스 초기화
 		}
 	}
 }
