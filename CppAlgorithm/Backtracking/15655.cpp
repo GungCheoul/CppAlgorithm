63d85caa87796080c010c3ac403e824f7526ff1a
GungCheoul <jkc3556@gmail.com>
2022-03-21 14:54:11 +0900

    backtracking

---
+++ CppAlgorithm/Backtracking/15655.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[10];
+int num[10];
+bool isused[10];
+
+void func(int cur) {
+	if (cur == m) {
+		for (int i = 0; i < m; i++)
+			cout << num[arr[i]] << " ";
+		cout << "\n";
+		return;
+	}
+	int s = 0;
+	if (cur != 0) s = arr[cur - 1] + 1;
+	for (int i = s; i < n; i++) {
+		if (isused[i]) continue;
+		arr[cur] = i;
+		isused[i] = true;
+		func(cur + 1);
+		isused[i] = false;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> num[i];
+	sort(num, num + n);
+	func(0);
+
+	return 0;
+}
