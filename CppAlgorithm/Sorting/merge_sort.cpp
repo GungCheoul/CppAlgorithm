c5e71d2797f65e5b7eee839b423165983b0d478a
GungCheoul <jkc3556@gmail.com>
2022-04-11 15:53:57 +0900

    merge sort basic frame

--- CppAlgorithm/Sorting/merge_sort.cpp
+++ CppAlgorithm/Sorting/merge_sort.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int n, m;
-int a[1000005], b[1000005], c[2000005];
+int n = 10;
+int arr[1000001] = { 15, 25, 22, 357, 16, 23, -53, 12, 46, 3 };
+int tmp[1000001]; // 병합 결과 임시로 저장할 배열
+
+void merge(int st, int en) {
+	int mid = (st + en) / 2;
+	// arr[st:mid], arr[mid:en]은 이미 정렬된 상태일 때,
+	// arr[st:mid]와 arr[mid:en]을 합치는 과정
+	int lidx = st;
+	int ridx = mid;
+	for (int i = st; i < en; i++) {
+		if (ridx == en) tmp[i] = arr[lidx++];
+		else if (lidx == mid) tmp[i] = arr[ridx++];
+		else if (arr[lidx] <= arr[ridx]) tmp[i] = arr[lidx++];
+		else tmp[i] = arr[ridx++];
+	}
+	for (int i = st; i < en; i++) arr[i] = tmp[i];
+}
+
+void merge_sort(int st, int en) { // arr[st:en]을 정렬
+	if (en == st + 1) return; // 길이가 1인 경우
+	int mid = (st + en) / 2;
+	merge_sort(st, mid); // arr[st:mid] 정렬
+	merge_sort(mid, st); // arr[mid:en] 정렬
+	merge(st, en); // arr[st:mid], arr[mid:en] 병합
+}
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> a[i];
-	for (int i = 0; i < m; i++) cin >> b[i];
-
-	int aidx = 0, bidx = 0;
-	for (int i = 0; i < n + m; i++) {
-		if (bidx == m) c[i] = a[aidx++];
-		else if (aidx == n) c[i] = b[bidx++];
-		else if (a[aidx] <= b[bidx]) c[i] = a[aidx++];
-		else c[i] = b[bidx++];
-	}
-	for (int i = 0; i < n + m; i++) cout << c[i] << ' ';
+	merge_sort(0, n);
+	for (int i = 0; i < n; i++) cout << arr[i] << ' ';
 
 	return 0;
 }
