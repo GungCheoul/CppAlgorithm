84af9cf41e1f21019c92a4c021daf17be84a874c
Administrator <Administrator@User -2022RLNHX>
2022-04-19 14:38:06 +0900

    list

---
+++ CppAlgorithm/LinkedList/5397.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		string L;
+		cin >> L;
+		list<char> pw; // result string
+		auto cursor = pw.begin(); // cursor start
+		for (auto k : L) {
+			if (k == '<') {
+				if (cursor != pw.begin())
+					cursor--;
+			}
+			else if (k == '>') {
+				if (cursor != pw.end())
+					cursor++;
+			}
+			else if (k == '-') {
+				if (cursor != pw.begin()) {
+					cursor--;
+					cursor = pw.erase(cursor);
+					// important
+					// after erase, cannot use the used iterator
+					// if wanna use after erasing,
+					// should save the return value of erase again
+				}
+			}
+			else pw.insert(cursor, k);
+		}
+		for (auto k : pw) {
+			if (k == '\0') cout << "null";
+			else
+				cout << k;
+
+		}
+		cout << '\n';
+	}
+	
+	return 0;
+}
